0298505d97368f7f90e9f6943177fc89c7496c4d
xaravind <aravindbasava1@gmail.com>
2024-05-12 19:06:39 +0530

    shell practice

--- 05-array.sh
+++ 05-array.sh
@@ -1,8 +1,8 @@
 #!/bin/abash
 
-MOVIES=["RRR","FIRE","WATER"]
+MOVIES=["RRR" "FIRE" "WATER"]
 
 
 echo "movie name is :${MOVIES[0]}
-echo "story  is :${MOVIES[1]} , ${MOVIES[2]}
+echo "story  is : ${MOVIES[1]} , ${MOVIES[2]}
 echo "total is : ${MOVIES[@]}
