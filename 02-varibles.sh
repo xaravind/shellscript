5ba04c38c9aa7b4ed63f3fdd2526e996fc11ede6
xaravind <aravindbasava1@gmail.com>
2024-05-12 18:37:21 +0530

    shell practice

---
+++ 02-varibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 :: $PERSON2 how are you?"
+echo "$PERSON2 :: I'm good $PERSON1, how abot you?"
+echo "$PERSON1 :: I'm fine too. How is your work"
+echo "$PERSON2 :: it's good"
