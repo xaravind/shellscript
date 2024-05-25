8617e4f5e0fe5d933cf35ec5e357aa01f7ab37e7
xaravind <aravindbasava1@gmail.com>
2024-05-25 12:54:59 +0530

    shell

--- 11.colours.sh
+++ 11.colours.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e\31m"
-G="\e\32m"
-N="\e\0m"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 VALIDATE () {
   if [ $? -ne 0 ]
