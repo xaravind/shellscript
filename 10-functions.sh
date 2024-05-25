a372f2bbe1fd7178401a3a7fd7b294270a6f3f01
xaravind <aravindbasava1@gmail.com>
2024-05-25 12:06:27 +0530

    mess

---
+++ 10-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+VALIDATE () {
+  if [ $? -ne 0 ]
+   then
+     echo "$2...failre"
+      exit 1
+    else
+    echo "$2...suceess"
+   fi
+}
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y
+VALDATE $? "INSTALLLING MYSQL"
+
+dnf install git -y
+VALDATE $? "INSTALLLING MYSQL"
