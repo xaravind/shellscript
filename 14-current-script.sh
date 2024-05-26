31bc3aee51a5cc1d5b374325f5c7edf011e2aa79
xaravind <aravindbasava1@gmail.com>
2024-05-26 16:31:02 +0530

    db

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,7 +5,7 @@ COURSE="Devops from  Current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-chmod +x /homr/ec2-user/shellscript/15-other-script.sh
+chmod +x /home/ec2-user/shellscript/15-other-script.sh
 
 #./15-other-script.sh
 
